c41c4b814485bc67cd0edda4c60d2df0af53470b
gabriel140282 <gkaakmor@myuax.com>
2024-10-29 20:09:59 +0100

    V1.0 (en construcción)

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "triple_pitagorico.h"
+
+int main() {
+
+    triple_pitagorico();
+
+    std::cout<<tripleta1[0]<<", "<<tripleta1[1]<<", "<<tripleta1[2]<<std::endl;
+    std::cout<<tripleta2[0]<<", "<<tripleta2[1]<<", "<<tripleta2[2]<<std::endl;
+    std::cout<<tripleta3[0]<<", "<<tripleta3[1]<<", "<<tripleta3[2]<<std::endl;
+    std::cout<<tripleta4[0]<<", "<<tripleta4[1]<<", "<<tripleta4[2]<<std::endl;
+
+    return 0;
+}
